6a8842c704618426aea0a5819e8cf6272e6ec15f
keen-coder <kknaur@gmail.com>
2024-09-04 15:32:42 -0400

    renamed hello_world.asm to first_program.asm

--- week03/first_program.asm
+++ week03/first_program.asm
@@ -1,14 +1,17 @@
 ; Keenan Knaur, CIT-344
-; Hello World Program
-; Demonstrates some basics of assembly programming.  Prints "Hello World!"
+; First Program
+; Demonstrates some basics of assembly programming.  
 
 
 section .data ;-----------------------------------------------------------------
 ; Define any constants or variables with known values here
 
 ;constant definitions
-NUM_CATS 	equ 	8
-COLOR 		equ 	"blue"
+NUM_CATS    equ   8
+COLOR       equ   "blue"
+
+;variable definitions
+
 ;-------------------------------------------------------------------------------
 
 
