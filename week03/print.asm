1e303a7d226c19eb80f3c0db6d6602ca72bd451e
keen-coder <kknaur@gmail.com>
2024-09-05 08:51:12 -0400

    Added print.asm example from Week 01

---
+++ week03/print.asm
@@ -0,0 +1,33 @@
+; Daniel Yoas
+; First Program
+; August 17, 2022
+
+section .data
+; Variables/Constants with known values go here.
+; This data will be stored within the program and on the hard drive.
+
+;LF	EQU	10
+;NULL	EQU	0
+
+msg:	db	"Hello World!", 0x0A, 0x00
+msglen:	EQU	$-msg
+
+section .bss
+; Undefined varibles until process execution go here
+
+section .text
+global _start
+_start:
+
+print:
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, msg
+	mov	rdx, msglen
+	syscall
+
+exit:
+	mov	rax, 60
+	mov	rdi, 0
+	syscall
+
